3a567f584843becff5e4f5f8f20498ff7591ab85
stoneyangxu <stoneyangxu@icloud.com>
2017-04-09 02:23:20 +0800

    init todo item component

--- src/app/todo/todo.module.ts
+++ src/app/todo/todo.module.ts
@@ -6,13 +6,14 @@ import { TodoListComponent } from './todo-list/todo-list.component';
 import { TodoMainComponent } from './todo-main/todo-main.component';
 import { TodoService } from 'app/todo/service/todo.service';
 import { NewTodoComponent } from './new-todo/new-todo.component';
+import { TodoItemComponent } from './todo-item/todo-item.component';
 
 @NgModule({
   imports: [
     CommonModule,
     TodoRoutingModule
   ],
-  declarations: [TodoListComponent, TodoMainComponent, NewTodoComponent],
+  declarations: [TodoListComponent, TodoMainComponent, NewTodoComponent, TodoItemComponent],
   providers: [
     TodoService
   ]
