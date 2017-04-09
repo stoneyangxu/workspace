40029afde38a13e302271939bd3d153152d02603
stoneyangxu <stoneyangxu@icloud.com>
2017-04-09 16:10:03 +0800

    redirect proxy to json-server

--- src/app/todo/service/todo.service.ts
+++ src/app/todo/service/todo.service.ts
@@ -1,10 +1,15 @@
-import {Injectable} from '@angular/core';
+import { Injectable } from '@angular/core';
 import * as moment from 'moment';
-import {TodoStatus} from 'app/todo/model/todo-status.enum';
-import {TodoItem} from 'app/todo/model/todo-item';
+import { TodoStatus } from 'app/todo/model/todo-status.enum';
+import { TodoItem } from 'app/todo/model/todo-item';
+import { Http } from '@angular/http';
+
+import 'rxjs/add/operator/toPromise';
 
 @Injectable()
 export class TodoService {
+
+  private todosUrl = 'api/todos';
 
   todoList: TodoItem[] = [
     {
@@ -21,14 +26,14 @@ export class TodoService {
     }
   ];
 
-  constructor() {
+  constructor(private http: Http) {
   }
 
   getTodoItemList(): Promise<TodoItem[]> {
-
-    this.todoList.sort();
-
-    return Promise.resolve(this.todoList);
+    return this.http.get(this.todosUrl)
+      .toPromise()
+      .then((response) => response.json().data as TodoItem[])
+      .catch((error: any) => Promise.reject(error.message || error));
   }
 
   getTodoItemById(id: number): Promise<TodoItem> {
