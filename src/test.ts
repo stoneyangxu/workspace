bdf6aa3bfe985f14600fe8bb53e060ef30223fff
stoneyangxu <stoneyangxu@icloud.com>
2017-04-30 00:33:38 +0800

    remove useless spec

--- src/test.ts
+++ src/test.ts
@@ -27,7 +27,8 @@ getTestBed().initTestEnvironment(
   platformBrowserDynamicTesting()
 );
 // Then we find all the tests.
-const context = require.context('./app/demo-components/my-dropdown/', true, /\.spec\.ts$/);
+const context = require.context('./app/', true, /\.spec\.ts$/);
+// const context = require.context('./app/demo-components/my-dropdown/', true, /\.spec\.ts$/);
 // And load the modules.
 context.keys().map(context);
 // Finally, start Karma to run the tests.
